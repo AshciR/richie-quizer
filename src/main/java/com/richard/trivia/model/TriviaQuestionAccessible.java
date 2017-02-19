742afbd79949208253861448d6fdb90d812ae771
Richard Walker <richard.walker.90@gmail.com>
2017-02-19 16:45:32 -0500

    Added Javadocs for the interface

--- src/main/java/com/richard/trivia/model/TriviaQuestionAccessible.java
+++ src/main/java/com/richard/trivia/model/TriviaQuestionAccessible.java
@@ -10,16 +10,49 @@ import java.util.Optional;
  */
 public interface TriviaQuestionAccessible {
 
+    /**
+     * Gets a trivia question from a known index location,
+     * if the index is within bounds.
+     *
+     * @param index the index position of the question.
+     * @return the trivia question if it exists.
+     */
     Optional<TriviaQuestion> getQuestionByIndex(long index);
 
+    /**
+     * Gets a trivia question based on its id,
+     * if the question exists.
+     *
+     * @param id the id of the trivia question.
+     * @return the trivia question if it exists.
+     */
     Optional<TriviaQuestion> getQuestionById(long id);
 
+    /**
+     * Gets a random trivia question
+     * @return a random trivia question.
+     */
     Optional<TriviaQuestion> getRandomQuestion();
 
+    /**
+     * Returns a sublist of questions from the database
+     * starting from a particular location.
+     * @param offset the location for which the sublist should start
+     * @return the sublist of questions
+     */
     List<TriviaQuestion> getQuestionList(long offset);
 
+    /**
+     * Gets a list of questions based on their ids.
+     * @param id the ids of the questions to be returned.
+     * @return the list of the questions whose ids were supplied.
+     */
     List<TriviaQuestion> getSpecifiedQuestionsList(long... id);
 
+    /**
+     * Gets number of question currently in the database.
+     * @return the number of questions in the database.
+     */
     long getQuestionListSize();
 
 }
